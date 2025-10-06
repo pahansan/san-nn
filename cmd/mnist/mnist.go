e2bdd9f05e65c332783157c2efbd819aca53da64
Kulik Pavel <pashakulik58@gmail.com>
2025-10-06 17:39:49 +0700

    Добавлена возможность тренировать нейронку до заданной точности

--- cmd/mnist/mnist.go
+++ cmd/mnist/mnist.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"san-nn/internal/nn"
 	"san-nn/internal/parser"
 )
@@ -24,43 +25,69 @@ func maxIndex(arr []float64) int {
 	return idx
 }
 
-func main() {
-	fmt.Println("Parsing...")
-	strs, _ := parser.ReadCSV("mnist_train.csv")
-	nums := parser.ParseLines(strs)
-	fmt.Println("Train...")
-	mnist := nn.NewNN([]int{784, 16, 16, 10})
-	mnist.InitWeightsRand()
-	for i, ex := range nums {
+func shuffle(slice [][]float64) {
+	for i := range slice {
+		j := rand.Intn(len(slice))
+		slice[i], slice[j] = slice[j], slice[i]
+	}
+}
+
+func prepareData(data [][]float64) {
+	for _, ex := range data {
 		input := ex[1:]
 		for j := range input {
 			input[j] = input[j] / 255
 		}
-		mnist.SetInput(input)
-		mnist.ForwardProp()
-		mnist.BackProp(formatTarget(int(ex[0])), 0.3)
-		if i%10000 == 0 {
-			cost, _ := mnist.GetCost(formatTarget(int(ex[0])))
-			fmt.Println("Iteration:", i, "Cost:", cost)
+	}
+}
+
+func countAccuracy(data [][]float64, model nn.NN) float64 {
+	correctCount := 0
+	for _, ex := range data {
+		input := ex[1:]
+		model.SetInput(input)
+		model.ForwardProp()
+		ans := maxIndex(model.GetOutput())
+		if ans == int(ex[0]) {
+			correctCount++
+		}
+	}
+	return float64(correctCount) / 10000 * 100
+
+}
+
+func main() {
+	fmt.Println("Parsing...")
+	strs, _ := parser.ReadCSV("mnist_train.csv")
+	train := parser.ParseLines(strs)
+	prepareData(train)
+	strs, _ = parser.ReadCSV("mnist_test.csv")
+	test := parser.ParseLines(strs)
+	prepareData(test)
+	fmt.Println("Train...")
+	mnist := nn.NewNN([]int{784, 32, 16, 10})
+	mnist.InitWeightsRand()
+	accuracy := 0.0
+	targetAccuracy := 95.0
+	for j := 0; accuracy <= targetAccuracy; j++ {
+		shuffle(train)
+		for i, ex := range train {
+			input := ex[1:]
+			mnist.SetInput(input)
+			mnist.ForwardProp()
+			mnist.BackProp(formatTarget(int(ex[0])), 0.1)
+			if i%10000 == 0 {
+				cost, _ := mnist.GetCost(formatTarget(int(ex[0])))
+				accuracy = countAccuracy(test, mnist)
+				fmt.Println("Iteration:", i+j*60000, "Cost:", cost, "Accuracy:", accuracy, "%")
+				if accuracy >= targetAccuracy {
+					break
+				}
+			}
 		}
 	}
 
 	fmt.Println("Validation...")
 	fmt.Println("Parsing...")
-	strs, _ = parser.ReadCSV("mnist_test.csv")
-	nums = parser.ParseLines(strs)
-	correctCount := 0
-	for _, ex := range nums {
-		input := ex[1:]
-		for j := range input {
-			input[j] = input[j] / 255
-		}
-		mnist.SetInput(input)
-		mnist.ForwardProp()
-		ans := maxIndex(mnist.GetOutput())
-		if ans == int(ex[0]) {
-			correctCount++
-		}
-	}
-	fmt.Println("Accuracy: ", float64(correctCount)/10000*100, "%")
+	fmt.Println("Accuracy: ", countAccuracy(test, mnist), "%")
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module san-nn
 
 go 1.25.1
+
+require fyne.io/fyne/v2 v2.6.3 // indirect
